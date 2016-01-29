7906eede123448b069f759da3c572d288f914430
Maxime Haineault <maxime.haineault@corp.ovh.ca>
2016-01-29 15:59:09 -0500

    Styles refactoring, fixed deprecation warnings Signed-off-by: Maxime Haineault <maxime.haineault@corp.ovh.ca>

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,4 +1,5 @@
 /*global angular,console*/
+
 angular.module('TatUi', [
     'tat.config',
     'ui.bootstrap',
@@ -25,18 +26,21 @@ angular.module('TatUi', [
     $locationProvider.html5Mode(true);
   })
 
-.config(function($httpProvider) {
-  'use strict';
-  $httpProvider.interceptors.push('TatEngineInterceptor');
+  .config(function($httpProvider) {
+    'use strict';
+    $httpProvider.interceptors.push('TatEngineInterceptor');
 })
 
 .config(function($translateProvider, $windowProvider) {
   'use strict';
   $translateProvider.useCookieStorage();
 
+  // http://angular-translate.github.io/docs/#/guide/19_security
+  $translateProvider.useSanitizeValueStrategy('sanitize');
+
   // set default and fallback languages
   $translateProvider.preferredLanguage('fr');
-  $windowProvider.$get().moment.lang('fr');
+  $windowProvider.$get().moment.locale('fr');
 
   // define translation loader
   $translateProvider.useLoader("$translatePartialLoader", {
@@ -64,7 +68,7 @@ angular.module('TatUi', [
 
 .run(function(Authentication) {
   'use strict';
-  moment.lang('fr', {
+  moment.locale('fr', {
     calendar: {
       lastDay: '[Yesterday], dddd MMM D H:mm:ss',
       sameDay: '[Today], H:mm:ss',
