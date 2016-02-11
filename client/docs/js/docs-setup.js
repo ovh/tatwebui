cb7a758262e59efb1fc1b9a115d26585ac722eee
Yvonnick Esnault <yvonnick.esnault@ovh.net>
2016-02-11 22:49:00 +0100

    doc: topic/truncate
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@ovh.net>

--- client/docs/js/docs-setup.js
+++ client/docs/js/docs-setup.js
@@ -361,7 +361,7 @@ NG_DOCS={
       "type": "resource",
       "moduleName": "TatUi",
       "shortDescription": "",
-      "keywords": "addadmingroup addadminuser addparameter addrogroup addrouser addrwgroup addrwuser api create function onetopic post promise removeadmingroup removeadminuser removeparameter removerogroup removerouser removerwgroup removerwuser resource return tatui updateparam"
+      "keywords": "addadmingroup addadminuser addparameter addrogroup addrouser addrwgroup addrwuser api create function onetopic post promise removeadmingroup removeadminuser removeparameter removerogroup removerouser removerwgroup removerwuser resource return tatui truncate updateparam"
     },
     {
       "section": "api",
